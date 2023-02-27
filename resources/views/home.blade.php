556f2b591a6dbf8f98af10d3a3cd2d16dc19cf5d
Ibraheem <ibraheem.alaoor@hotmail.com>
2023-02-27 17:20:29 +0200

    upp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -219,122 +219,44 @@
     </div>
 
 
-    <div id="services" class="our-portfolio section">
+
+    <div id="services" class="our-services section">
         <div class="container">
             <div class="row">
-                <div class="col-lg-5">
-                    <div class="section-heading wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.3s">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="section-heading wow bounceIn" data-wow-duration="1s" data-wow-delay="0.2s">
                         <h6>Our Services</h6>
                         <h2>Discover Our Recent <em>Services</em> For <span>Social Media Platforms</span></h2>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="container-fluid wow fadeIn" data-wow-duration="1s" data-wow-delay="0.7s">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12">
-                    <div class="loop owl-carousel">
-                        <div class="item">
-                            @foreach ($platforms as $platform)
-                                <div class="portfolio-item">
-                                    <div class="thumb">
-                                        <img src="{{$platform->getLogo()}}" alt="">
-                                        <div class="hover-content">
-                                            <div class="inner-content">
-                                                <a href="{{route('platform_services' , encrypt($platform->id))}}">
-                                                    <h4>{{ $platform->name }}</h4>
-                                                </a>
-                                            </div>
+                @foreach ($platforms as $platform)
+                    <div class="col-lg-6 text-center">
+                        <div class="service-item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+                            <div class="row">
+                                <a href="{{ route('platform_services', encrypt($platform->id)) }}">
+                                    <div class="col-lg-12">
+                                        <div class="icon">
+                                            <img src="{{ $platform->getLogo() }}" alt="">
+                                        </div>
+                                        <div class="right-content">
+                                            <h4>{{ $platform->name }}</h4>
                                         </div>
                                     </div>
-                                </div>
-                            @endforeach
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
 
     <div id="contact" class="contact-us section">
         <div class="container">
-            {{-- <div class="row">
-                <div class="col-lg-12 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.25s">
-                    <form id="contact" action="" method="post">
-                        <div class="row">
-                            <div class="col-lg-6 offset-lg-3">
-                                <div class="section-heading">
-                                    <h6>Contact Us</h6>
-                                    <h2>Fill Out The Form Below To <span>Get</span> In <em>Touch</em> With Us</h2>
-                                </div>
-                            </div>
-                            <div class="col-lg-9">
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <fieldset>
-                                            <input type="name" name="name" id="name" placeholder="Name"
-                                                autocomplete="on" required>
-                                        </fieldset>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <fieldset>
-                                            <input type="surname" name="surname" id="surname" placeholder="Surname"
-                                                autocomplete="on" required>
-                                        </fieldset>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <fieldset>
-                                            <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*"
-                                                placeholder="Your Email" required="">
-                                        </fieldset>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <fieldset>
-                                            <input type="subject" name="subject" id="subject" placeholder="Subject"
-                                                autocomplete="on">
-                                        </fieldset>
-                                    </div>
-                                    <div class="col-lg-12">
-                                        <fieldset>
-                                            <textarea name="message" type="text" class="form-control" id="message" placeholder="Message" required=""></textarea>
-                                        </fieldset>
-                                    </div>
-                                    <div class="col-lg-12">
-                                        <fieldset>
-                                            <button type="submit" id="form-submit" class="main-button ">Send Message
-                                                Now</button>
-                                        </fieldset>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-3">
-                                <div class="contact-info">
-                                    <ul>
-                                        <li>
-                                            <div class="icon">
-                                                <img src="assets/images/contact-icon-01.png" alt="email icon">
-                                            </div>
-                                            <a href="#">info@company.com</a>
-                                        </li>
-                                        <li>
-                                            <div class="icon">
-                                                <img src="assets/images/contact-icon-02.png" alt="phone">
-                                            </div>
-                                            <a href="#">+001-002-0034</a>
-                                        </li>
-                                        <li>
-                                            <div class="icon">
-                                                <img src="assets/images/contact-icon-03.png" alt="location">
-                                            </div>
-                                            <a href="#">26th Street, Digital Villa</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div> --}}
         </div>
     </div>
 @endsection
