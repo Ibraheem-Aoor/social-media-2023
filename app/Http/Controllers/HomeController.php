80914b6b4e025de473fc7636787fc69a7f40e1b8
Ibraheem <ibraheem.alaoor@hotmail.com>
2023-02-27 21:31:01 +0200

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -76,7 +76,7 @@ class HomeController extends Controller
     public function taskComplete()
     {
         $temp_user = TempUser::find(session()->get('database_session_id'));
-        if(@$temp_user->visited)
+        if(@$temp_user->visited && session()->has('has_visited') && session()->get('has_visited') == 1)
         {
             $data['service_id'] =     $temp_user->service_id;
             $data['form_route'] =   route("user_url.save" , encrypt($data['service_id']));
